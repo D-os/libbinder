4a7f4d02cd9dd7f2f946d7859091d14f70929335
Christopher Wiley <wiley@google.com>
2016-04-11 11:09:37 -0700

    libbinder: Replace ScopedFd with base::unique_fd
    unique_fd is a more canonical construct going forward.
    
    Bug: 27804373
    Test: checkbuilds pass on aosp x86_64-eng
    
    Change-Id: I8c1f202f1a6d646b5468f8c08fdcab7ea7f3178b

--- Android.mk
+++ Android.mk
@@ -44,7 +44,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libbinder
-LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
+LOCAL_SHARED_LIBRARIES := libbase liblog libcutils libutils
+LOCAL_EXPORT_SHARED_LIBRARY_HEADERS := libbase libutils
 
 LOCAL_CLANG := true
 LOCAL_SANITIZE := integer
@@ -59,7 +60,8 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libbinder
-LOCAL_STATIC_LIBRARIES += libutils
+LOCAL_STATIC_LIBRARIES := libbase libutils
+LOCAL_EXPORT_STATIC_LIBRARY_HEADERS := libbase libutils
 LOCAL_SRC_FILES := $(sources)
 ifneq ($(TARGET_USES_64_BIT_BINDER),true)
 ifneq ($(TARGET_IS_64_BIT),true)
