ee81ac12a4a294b3cf60f3b7f358812993f1860e
Luis A. Lozano <llozano@google.com>
2017-09-19 17:33:48 -0700

    Fix "use of memory after freed" warning.
    Static analyzer gets confused in the code to free a linked list.
    To avoid confusion add an "assert" to tell the analyzer there is no
    alias between the memory being deleted and accessed.
    
    Bug: b/27101951
    Test: Warning is gone.
    Change-Id: Ia6661ee76908c2025b5e1a764d25ba3e11bf44c8

--- MemoryDealer.cpp
+++ MemoryDealer.cpp
@@ -289,7 +289,15 @@ SimpleBestFitAllocator::SimpleBestFitAllocator(size_t size)
 SimpleBestFitAllocator::~SimpleBestFitAllocator()
 {
     while(!mList.isEmpty()) {
-        delete mList.remove(mList.head());
+        chunk_t* removed = mList.remove(mList.head());
+#ifdef __clang_analyzer__
+        // Clang static analyzer gets confused in this loop
+        // and generates a false positive warning about accessing
+        // memory that is already freed.
+        // Add an "assert" to avoid the confusion.
+        LOG_ALWAYS_FATAL_IF(mList.head() == removed);
+#endif
+        delete removed;
     }
 }
 
