8dfa74a3918a56408fa182b799a7e9fb6e805b8a
Yunfan Chen <yunfanc@google.com>
2018-03-29 16:52:09 +0900

    Reduce the polling interval in getService() to 100ms
    The 1 second polling interval is too long and 100ms interval can reduce
    the boot time, especially for ARC++. The test results can be found in
    b/30892329.
    
    This CL also adds some ALOGs to show the service start failure as well
    as to avoid spam.
    
    Bug: 38432898
    Test: Services can still start. Test cheets_PerfBootServer for ARC++.
    Change-Id: I9c0da9ef8a18a0e1432c39d98a34377bb66c5d85

--- include/private/binder/Static.h
+++ include/private/binder/Static.h
@@ -41,5 +41,6 @@ extern sp<IServiceManager> gDefaultServiceManager;
 #ifndef __ANDROID_VNDK__
 extern sp<IPermissionController> gPermissionController;
 #endif
+extern bool gSystemBootCompleted;
 
 }   // namespace android
