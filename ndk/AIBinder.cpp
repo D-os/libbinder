6f8629e6df1972b03c898178e957ab9fcf536e2b
Steven Moreland <smoreland@google.com>
2018-09-05 17:07:14 -0700

    libbinder_ndk: warning for transact on local objs
    Since a transaction on these means that you are parceling/unparceling
    things when you could be calling it in process.
    
    Bug: 111445392
    Test: runtests.sh
    Change-Id: I620889ecd565553900b087b638ed29ffd98cedd2

--- ndk/AIBinder.cpp
+++ ndk/AIBinder.cpp
@@ -269,6 +269,10 @@ binder_status_t AIBinder_prepareTransaction(AIBinder* binder, AParcel** in) {
         return EX_ILLEGAL_STATE;
     }
 
+    if (!binder->isRemote()) {
+        LOG(WARNING) << "A binder object at " << binder << " is being transacted on, however, this object is in the same process as its proxy. Transacting with this binder is expensive compared to just calling the corresponding functionality in the same process.";
+    }
+
     *in = new AParcel(binder);
     binder_status_t status = (**in)->writeInterfaceToken(clazz->getInterfaceDescriptor());
     if (status != EX_NONE) {
