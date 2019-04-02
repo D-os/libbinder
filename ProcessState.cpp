aad205b29e98510567d9b37bdc9e28197c1832bb
Steven Moreland <smoreland@google.com>
2019-04-01 18:52:30 -0700

    libbinder: on host
    The usecase is unit testing things which implement binder interfaces.
    
    Bug: 124524556
    Test: TH runs all relevant unit tests
    Change-Id: I8caba1290da6d0354a7736a26d1dad7b6ab852e6

--- ProcessState.cpp
+++ ProcessState.cpp
@@ -189,7 +189,8 @@ ssize_t ProcessState::getKernelReferences(size_t buf_count, uintptr_t* buf)
 }
 
 void ProcessState::setCallRestriction(CallRestriction restriction) {
-    LOG_ALWAYS_FATAL_IF(IPCThreadState::selfOrNull(), "Call restrictions must be set before the threadpool is started.");
+    LOG_ALWAYS_FATAL_IF(IPCThreadState::selfOrNull() != nullptr,
+        "Call restrictions must be set before the threadpool is started.");
 
     mCallRestriction = restriction;
 }
