df7e94dd2c4d8cb8aed03d4fbe52c4d1c2ab0102
Steven Moreland <smoreland@google.com>
2019-09-20 11:24:28 -0700

    libbinder: __ANDROID_HOST__ => !__ANDROID__
    Slightly simplifies config and probably makes more sense.
    
    Bug: 124524556
    Test: mma (continues to build on host)
    Change-Id: Id11dbc25b581919ce8814c9c9bca191071434142

--- include/private/binder/binder_module.h
+++ include/private/binder/binder_module.h
@@ -24,7 +24,7 @@ namespace android {
 /* obtain structures and constants from the kernel header */
 
 // TODO(b/31559095): bionic on host
-#ifdef __ANDROID_HOST__
+#ifndef __ANDROID__
 #define __packed __attribute__((__packed__))
 #endif
 
