37743098605ce667fde341bf76859a0f9f8a1d7f
Steven Moreland <smoreland@google.com>
2020-09-21 18:03:27 +0000

    Reland "libbinder: ignore CallRestriction for magic 0 cmd"
    This reverts commit 41a4c36712b23aff58d9305e4191dc18d143f6bd.
    
    This fixes the CallRestriction issue, but removes the added test
    which has problems in another context and will be fixed separately.
    
    Reason for revert: refix b/167302413
    Testing issue will be handled separately in b/168942807.
    
    Fixes: 167302413
    
    Change-Id: Iedc1ff73d086c973bfcab511ace26d0ec2291040

--- IPCThreadState.cpp
+++ IPCThreadState.cpp
@@ -448,6 +448,14 @@ int32_t IPCThreadState::getLastTransactionBinderFlags() const
     return mLastTransactionBinderFlags;
 }
 
+void IPCThreadState::setCallRestriction(ProcessState::CallRestriction restriction) {
+    mCallRestriction = restriction;
+}
+
+ProcessState::CallRestriction IPCThreadState::getCallRestriction() const {
+    return mCallRestriction;
+}
+
 void IPCThreadState::restoreCallingIdentity(int64_t token)
 {
     mCallingUid = (int)(token>>32);
