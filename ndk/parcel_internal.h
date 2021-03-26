54b443279fcceab498335d16f55abac55d6af1d2
Steven Moreland <smoreland@google.com>
2021-03-26 02:04:16 +0000

    libbinder_ndk: call markForBinder internally
    To support using libbinder_ndk w/ sockets.
    
    This doesn't make a lot of sense without NDK APIs to get ahold of
    binders, but platform utilities like the test here don't need those.
    
    Bug: 167966510
    Test: binderRpcTest
    Change-Id: I63739a0d971e05b71908140caf6c6221fde48564

--- ndk/parcel_internal.h
+++ ndk/parcel_internal.h
@@ -27,10 +27,13 @@ struct AParcel {
     const ::android::Parcel* get() const { return mParcel; }
     ::android::Parcel* get() { return mParcel; }
 
-    explicit AParcel(const AIBinder* binder)
-        : AParcel(binder, new ::android::Parcel, true /*owns*/) {}
-    AParcel(const AIBinder* binder, ::android::Parcel* parcel, bool owns)
-        : mBinder(binder), mParcel(parcel), mOwns(owns) {}
+    explicit AParcel(AIBinder* binder) : AParcel(binder, new ::android::Parcel, true /*owns*/) {}
+    AParcel(AIBinder* binder, ::android::Parcel* parcel, bool owns)
+        : mBinder(binder), mParcel(parcel), mOwns(owns) {
+        if (binder != nullptr) {
+            parcel->markForBinder(binder->getBinder());
+        }
+    }
 
     ~AParcel() {
         if (mOwns) {
@@ -38,7 +41,7 @@ struct AParcel {
         }
     }
 
-    static const AParcel readOnly(const AIBinder* binder, const ::android::Parcel* parcel) {
+    static const AParcel readOnly(AIBinder* binder, const ::android::Parcel* parcel) {
         return AParcel(binder, const_cast<::android::Parcel*>(parcel), false);
     }
 
