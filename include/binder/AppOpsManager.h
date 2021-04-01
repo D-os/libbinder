20d60669dea4477ac4bb35ce994f5401bef1e72f
Jeff Sharkey <jsharkey@android.com>
2021-04-01 13:53:20 -0600

    New BLUETOOTH_ADVERTISE manifest permission.
    This change is part of defining a distinct BLUETOOTH_ADVERTISE
    permission to guard the BluetoothLeAdvertiser APIs, since that's a
    distinct enough of an operation from SCAN and CONNECT.  It'll
    continue to be covered under the general "Nearby devices" runtime
    permission group.
    
    Bug: 181813006
    Test: atest CtsPermission2TestCases
    Test: atest CtsPermission3TestCases
    Change-Id: I3b7f8def4dcb325e4b0c090fb9cb89e2e8dac114

--- include/binder/AppOpsManager.h
+++ include/binder/AppOpsManager.h
@@ -144,7 +144,9 @@ public:
         OP_MANAGE_MEDIA = 110,
         OP_BLUETOOTH_CONNECT = 111,
         OP_UWB_RANGING = 112,
-        _NUM_OP = 113
+        OP_ACTIVITY_RECOGNITION_SOURCE = 113,
+        OP_BLUETOOTH_ADVERTISE = 114,
+        _NUM_OP = 115
     };
 
     AppOpsManager();
